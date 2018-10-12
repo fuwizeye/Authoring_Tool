446ce22e70a9ee59053002ff40e84ede839ffa8f
NebiyouTen <nebiyoutenager@gmail.com>
2018-10-12 12:44:48 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import argparse
+import sys
+import os
+import numpy as np
+import bmp_io_c
+import math
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Authroing tool')
+
+
+
+
+if __name__ == '__main__':
+    main(sys.argv)
